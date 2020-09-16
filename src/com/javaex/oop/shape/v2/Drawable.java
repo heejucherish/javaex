70116d9f23484d4b5304a859432bfa333eaa7e41
heejucherish <sul1952@korea.ac.kr>
2020-09-16 16:58:56 +0900

    객체지향 정리

---
+++ src/com/javaex/oop/shape/v2/Drawable.java
@@ -0,0 +1,6 @@
+package com.javaex.oop.shape.v2;
+
+public interface Drawable {
+	//	추상 메서드이지만 abstract는 쓰지 않음
+	public void draw();
+}
---
+++ src/com/javaex/oop/shape/v2/ShapeApp.java
@@ -0,0 +1,12 @@
+package com.javaex.oop.shape.v2;
+
+public class ShapeApp {
+
+	public static void main(String[] args) {
+		Shape[] objects = new Shape[3];
+		
+		Ci
+
+	}
+
+}
